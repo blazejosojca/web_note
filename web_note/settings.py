45143a5f05acab744e322d02adc3fffe03d1b8bb
blazejosojca <blazejosojca@gmail.com>
2018-06-07 20:48:38 +0200

    modified settings, debug and hosts for production

--- web_note/settings.py
+++ web_note/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0)xczigie8kyfr!mnhef9ft9^+#^t)*8g-4($#8j6&r9+gxfc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
